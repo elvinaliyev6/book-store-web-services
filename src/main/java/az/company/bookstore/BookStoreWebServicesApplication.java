e36162a473c4717099ccfe3434af203835b33c1d
elvinaliyev6 <elvinaliyevinfo@gmail.com>
2022-10-06 12:05:51 +0400

    first commit

---
+++ src/main/java/az/company/bookstore/BookStoreWebServicesApplication.java
@@ -0,0 +1,13 @@
+package az.company.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreWebServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreWebServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/company/bookstore/BookStoreWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package az.company.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreWebServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
